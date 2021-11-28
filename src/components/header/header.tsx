b01ec272e50aaa79ef6ce50defae27e7121796bb
Dan Bednarczyk <dpbednarczyk@gmail.com>
2021-11-28 14:54:27 -0800

    cleanups

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -19,25 +19,36 @@ const Header: React.FC<any> = ({ pageName }) => {
                 <div className="right">
                     <ul className="icon-list">
                         <li>
-                            <a href="">
+                            <a target="_blank" href="https://www.linkedin.com/in/danielbednarczyk/">
                                 <Icon id="linkedin" />
                             </a>
                         </li>
                         <li>
-                            <a href="">
+                            <a target="_blank" href="https://github.com/dan9418/">
                                 <Icon id="github" />
                             </a>
                         </li>
                         <li>
-                            <a href="">
-                                <Icon id="bandcamp" />
-                            </a>
-                        </li>
-                        <li>
-                            <a href="">
+                            <a target="_blank" href="https://www.instagram.com/pineapple_dan_creates/">
                                 <Icon id="instagram" />
                             </a>
                         </li>
+                        <li>
+                            <a target="_blank" href="https://open.spotify.com/artist/04pnwkbpBItHPWSuL5UAYY?si=VBizxZcESVCOIhPmChdUuQ">
+                                <Icon id="spotify" />
+                            </a>
+                        </li>
+                        {/*<li>
+                            <a target="_blank" href="https://music.apple.com/us/artist/pineapple-dan/1590781822">
+                                <Icon id="apple" />
+                            </a>
+                        </li>*/}
+                        <li>
+                            <a target="_blank" href="https://pineapple-dan.bandcamp.com/">
+                                <Icon id="bandcamp" />
+                            </a>
+                        </li>
+
                     </ul>
                     <div className="divider" />
                     <ul className="link-list">
--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -12,18 +12,18 @@ const ResumePage = () => {
 
                 <h3>Music</h3>
                 <ul>
-                    <li><a href="https://open.spotify.com/artist/04pnwkbpBItHPWSuL5UAYY?si=VBizxZcESVCOIhPmChdUuQ">Spotify</a></li>
-                    <li><a href="https://music.apple.com/us/artist/pineapple-dan/1590781822">Apple Music</a></li>
-                    <li><a href="https://pineapple-dan.bandcamp.com/">Bandcamp</a></li>
+                    <li><a target="_blank" href="https://open.spotify.com/artist/04pnwkbpBItHPWSuL5UAYY?si=VBizxZcESVCOIhPmChdUuQ">Spotify</a></li>
+                    <li><a target="_blank" href="https://music.apple.com/us/artist/pineapple-dan/1590781822">Apple Music</a></li>
+                    <li><a target="_blank" href="https://pineapple-dan.bandcamp.com/">Bandcamp</a></li>
                 </ul>
                 <h3>Art</h3>
                 <ul>
-                    <li><a href="https://www.instagram.com/pineapple_dan_creates/">Instagram</a></li>
+                    <li><a target="_blank" href="https://www.instagram.com/pineapple_dan_creates/">Instagram</a></li>
                 </ul>
                 <h3>Software</h3>
                 <ul>
-                    <li><a href="https://github.com/dan9418/">GitHub</a></li>
-                    <li><a href="https://www.linkedin.com/in/danielbednarczyk/">LinkedIn</a></li>
+                    <li><a target="_blank" href="https://github.com/dan9418/">GitHub</a></li>
+                    <li><a target="_blank" href="https://www.linkedin.com/in/danielbednarczyk/">LinkedIn</a></li>
                 </ul>
 
                 {/*<h2>Play What</h2>
