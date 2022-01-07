32be19a03c569bc0b79eb63076f42fc21b76165b
Dan Bednarczyk <dpbednarczyk@gmail.com>
2022-01-07 08:01:01 -0800

    comment ssr file

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,36 +1,36 @@
-import React from "react";
-import Page from "./src/components/page/page";
+// import React from "react";
+// import Page from "./src/components/page/page";
 
-/*export const wrapRootElement = ({ element, ...props }) => {
-    return (
-        <ErrorBoundary>
-            {element}
-        </ErrorBoundary>
-    )
-}*/
+// /*export const wrapRootElement = ({ element, ...props }) => {
+//     return (
+//         <ErrorBoundary>
+//             {element}
+//         </ErrorBoundary>
+//     )
+// }*/
 
-const PAGE_CONFIG = {
-    '/': {
-        isHeaderVisible: false,
-        pageTitle: 'Home',
-        pageName: 'index'
-    },
-    '/resume/': {
-        isHeaderVisible: true,
-        pageTitle: 'Resume',
-        pageName: 'resume'
-    },
-    '/portfolio/': {
-        isHeaderVisible: true,
-        pageTitle: 'Portfolio',
-        pageName: 'portfolio'
-    }
-}
+// const PAGE_CONFIG = {
+//     '/': {
+//         isHeaderVisible: false,
+//         pageTitle: 'Home',
+//         pageName: 'index'
+//     },
+//     '/resume/': {
+//         isHeaderVisible: true,
+//         pageTitle: 'Resume',
+//         pageName: 'resume'
+//     },
+//     '/portfolio/': {
+//         isHeaderVisible: true,
+//         pageTitle: 'Portfolio',
+//         pageName: 'portfolio'
+//     }
+// }
 
-export const wrapPageElement = ({ element, props }) => {
-    return (
-        <Page {...PAGE_CONFIG[props.path]}>
-            {element}
-        </Page>
-    );
-}
+// export const wrapPageElement = ({ element, props }) => {
+//     return (
+//         <Page {...PAGE_CONFIG[props.path]}>
+//             {element}
+//         </Page>
+//     );
+// }
