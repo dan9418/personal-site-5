786b00fd934dc24ca2dbc25d6d14b7e68eb61f35
Dan Bednarczyk <dpbednarczyk@gmail.com>
2022-02-27 12:57:58 -0800

    disable ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,32 +1,34 @@
-import React from "react";
-import Page from "./src/components/page/page";
-import './src/styles/global.scss';
+// import React from "react";
+// import Page from "./src/components/page/page";
+// import './src/pages/index.scss';
+// import './src/pages/resume.scss';
+// import './src/styles/global.scss';
 
-/*export const wrapRootElement = ({ element, ...props }) => {
-    return (
-        <ErrorBoundary>
-            {element}
-        </ErrorBoundary>
-    )
-}*/
+// /*export const wrapRootElement = ({ element, ...props }) => {
+//     return (
+//         <ErrorBoundary>
+//             {element}
+//         </ErrorBoundary>
+//     )
+// }*/
 
-const PAGE_CONFIG = {
-    '/': {
-        isHeaderVisible: false,
-        pageTitle: '@pineapple_dan',
-        pageName: 'index'
-    },
-    '/resume/': {
-        isHeaderVisible: true,
-        pageTitle: 'Resume',
-        pageName: 'resume'
-    }
-}
+// const PAGE_CONFIG = {
+//     '/': {
+//         isHeaderVisible: false,
+//         pageTitle: '@pineapple_dan',
+//         pageName: 'index'
+//     },
+//     '/resume/': {
+//         isHeaderVisible: true,
+//         pageTitle: 'Resume',
+//         pageName: 'resume'
+//     }
+// }
 
-export const wrapPageElement = ({ element, props }) => {
-    return (
-        <Page {...PAGE_CONFIG[props.path]}>
-            {element}
-        </Page>
-    );
-}
+// export const wrapPageElement = ({ element, props }) => {
+//     return (
+//         <Page {...PAGE_CONFIG[props.path]}>
+//             {element}
+//         </Page>
+//     );
+// }
